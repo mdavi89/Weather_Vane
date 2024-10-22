6283ed7a98d880ea0d93309981a7f8f6aa5d023d
Matthew Davis <mdavi359@yahoo.com>
2024-10-22 15:39:25 -0400

    Added addCity method to HistoryService

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -28,7 +28,6 @@ class HistoryService {
   async getCities() {return await this.read().then((cities) => {
       let parsedCities: City[];
 
-      // If states isn't an array or can't be turned into one, send back a new empty array
       try {
         parsedCities = [].concat(JSON.parse(cities));
       } catch (err) {
@@ -38,7 +37,23 @@ class HistoryService {
       return parsedCities;
     });}
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {if (!city) {
+      throw new Error('City cannot be blank');
+    }
+
+    // Add a unique id to the state using uuid package
+    const newCity: City = { name: city, id: uuidv4() };
+
+    // Get all cities, add the new city, write all the updated cities, return the newCity
+    return await this.getCities()
+      .then((cities) => {
+        if (cities.find((index) => index.name === city)) {
+          return cities;
+        }
+        return [...cities, newCity];
+      })
+      .then((updatedCities) => this.write(updatedCities))
+      .then(() => newCity);}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
