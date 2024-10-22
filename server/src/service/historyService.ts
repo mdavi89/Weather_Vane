e05c6e7fb38319e382260e6f8db8b2b9d898b988
Matthew Davis <mdavi359@yahoo.com>
2024-10-22 15:41:25 -0400

    Added removeCity method to HistoryService

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -55,7 +55,9 @@ class HistoryService {
       .then((updatedCities) => this.write(updatedCities))
       .then(() => newCity);}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  async removeCity(id: string) {return await this.getCities()
+      .then((cities) => cities.filter((city) => city.id !== id))
+      .then((filteredCities) => this.write(filteredCities));}
 }
 
 export default new HistoryService();
