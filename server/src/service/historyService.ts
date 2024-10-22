22b4595572c735dc6daeaceaf1fdac6ee93b8392
Matthew Davis <mdavi359@yahoo.com>
2024-10-22 15:24:45 -0400

    Added the City class to historyService.ts

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,4 +1,13 @@
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
+
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
