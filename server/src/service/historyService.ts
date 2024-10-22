21aa0467b69fb0b259d190d80082b715a0da0f1c
Matthew Davis <mdavi359@yahoo.com>
2024-10-22 15:30:22 -0400

    Added read function to HistoryService

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,3 +1,6 @@
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
+
 // TODO: Define a City class with name and id properties
 class City {
   name: string;
@@ -12,7 +15,11 @@ class City {
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+      return await fs.readFile('db/db.json', {
+      flag: 'a+',
+      encoding: 'utf8',
+    });}
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
